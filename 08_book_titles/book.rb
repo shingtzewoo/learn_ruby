3a42701d4a7206e9c2985030df5bd05cc721b29b
shingtzewoo@gmail.com <shingtzewoo@gmail.com>
2016-06-11 17:39:17 -0700

    modified solution

--- 08_book_titles/book.rb
+++ 08_book_titles/book.rb
@@ -2,17 +2,16 @@ class Book
   attr_accessor :title
 
   def title
-    array2 = [] #this array is for pushing the final fixed title
     array = @title.split(' ') #this array is to hold the split title
     array.each_with_index do |e, i|
       if i == 0 or e == "i"
-        array2.push e.capitalize
+        e.capitalize!
       elsif e == "a" or e == "the" or e == "an" or e == "and" or e == "in" or e == "the" or e == "of"
-        array2.push e
+        e
       else
-        array2.push e.capitalize
+        e.capitalize!
       end
     end
-    return array2.join(' ') #returns the finished title
+    return array.join(' ') #returns the finished title
   end
 end
